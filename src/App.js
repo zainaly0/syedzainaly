53837a3e9544a4c5d665c598dd2107425152f73e
Zaid Aly <zaidk4076@gmail.com>
2023-11-16 23:06:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from './Components/Header/Header'
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+// import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import ScrollUp from "./Components/ScrollUp.js/ScrollUp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* header components  */}
+      <Header />
+      <main>
+        <Home />
+        <About />
+        {/* <Contact /> */}
+      </main>
+      {/* footer components  */}
+      <Footer />
+      <ScrollUp />
     </div>
   );
 }
