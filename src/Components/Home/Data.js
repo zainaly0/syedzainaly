e8faf60c84e19d4f114157b011a76a8fab7568a2
Zaid Aly <85671033+zainaly0@users.noreply.github.com>
2023-12-26 19:22:34 +0530

    Update Data.js

--- src/Components/Home/Data.js
+++ src/Components/Home/Data.js
@@ -55,7 +55,7 @@ const Data = () => {
                   ></path>
             </svg>  
         </h1>
-        <h3 className="home__subtitle">Advocate, Terrorist Lawyer<br />Engineer, Software Developer </h3>
+        <h3> Advocate ⚖️ <br />Engineer 💻 </h3>
         <p className="home__description">I'm creative designer based in Khurasan from Masrik, and I'm very passionate and dedicated to my work</p>
 
         <a href="contact" className="button button--flex">
